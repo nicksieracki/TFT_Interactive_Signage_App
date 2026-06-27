f0e4dcc604db92a245b81863dafba021ccf5be29
Kenneth Vu <186969144+kvu-ucla@users.noreply.github.com>
2026-06-26 21:17:52 -0700

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,65 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, type ProxyOptions } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+//////////////////////////////////////////////////////////////
+///////////////////   Proxy Configuration   //////////////////
+//////////////////////////////////////////////////////////////
+
+/** FQDN to proxy requests. i.e. No protocol and path should be in the value */
+const domain = 'placeos-prod.avit.it.ucla.edu';
+/** Whether the proxied endpoints use SSL */
+const secure = true;
+/** Whether the SSL certificate used is valid on the internet */
+const valid_ssl = true;
+
+const context = [
+  '/control',
+  '/auth',
+  '/api',
+  '/styles',
+  '/scripts',
+  '/login',
+  '/backoffice',
+  '/r',
+];
+const ws_context = ['/control/websocket', '/api'];
+
+const PROXY_MAP: Record<string, ProxyOptions> = {};
+
+const baseConfig: ProxyOptions = {
+  target: `http${secure ? 's' : ''}://${domain}`,
+  secure: valid_ssl,
+  changeOrigin: true,
+};
+
+context.forEach((path) => {
+  PROXY_MAP[path] = { ...baseConfig };
+});
+
+ws_context.forEach((path) => {
+  PROXY_MAP[path] = { ...baseConfig, ws: true };
+});
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@placeos/ts-client/dist/oauth-resp.html',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   base: './',
   server: {
-    port: 4200,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+    proxy: PROXY_MAP,
   },
 });
